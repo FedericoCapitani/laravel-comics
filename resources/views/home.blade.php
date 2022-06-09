2b310a7b76b89c219348c98b09c1d09d257c3b81
FedericoCapitani <federico.capitani@outlook.it>
2022-06-09 17:04:39 +0200

    complete header and start home section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Welcome</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis illo dolorum consequuntur vitae doloremque repellendus voluptatum deleniti saepe cum nulla. </p>
+<div class="jumbotron">
+    <img src="{{asset('img/jumbotron.jpg')}}" alt="">
+</div>
+
+<div class="bg_main">
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+            <div class="col-2">
+                <img src="{{asset('img/')}}" alt="">
+                <p>ACTION COMICS</p>
+            </div>
+    </div>
+</div>
+
+</div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,12 @@
 <header id="site_header">
-    <div class="top d-flex">
-        <p>DC POWER VISA</p>
-        <p>ADDITIONAL DC SITES</p>
+    <div class="top">
+        <div class="container d-flex justify-content-end">
+            <p>DC POWER VISA</p>
+            <p>ADDITIONAL DC SITES</p>
+        </div>
     </div>
     <nav class="nav justify-content-center">
+        <img src="{{asset('img/dc-logo.png')}}" alt="">
         <a class="nav-link active" href="">CHARACTERS</a>
         <a class="nav-link" href="">COMICS</a>
         <a class="nav-link" href="">MOVIES</a>
@@ -15,8 +18,8 @@
         <a class="nav-link" href="">NEWS</a>
         <a class="nav-link" href="">SHOP</a>
         <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <input class="form-control me-2 margin_tb_2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-primary margin_tb_2" type="submit">Search</button>
         </form>
     </nav>
 </header>
