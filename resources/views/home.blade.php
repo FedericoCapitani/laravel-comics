dce51e2ff4e872d44aa0199a6c8a4f8e8c374e50
FedericoCapitani <federico.capitani@outlook.it>
2022-06-09 14:44:31 +0200

    install npm and bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+
 </head>
+
 <body>
-    <h1>prova</h1>
+    <header></header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer></footer>
+
 </body>
+
 </html>
