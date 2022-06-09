af311fbc2602870fef6f7e43b19f417d0993fd29
FedericoCapitani <federico.capitani@outlook.it>
2022-06-09 16:11:07 +0200

    add nav links and top to header section

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,8 @@
-<header>
-
+<header id="site_header">
+    <div class="top d-flex">
+        <p>DC POWER VISA</p>
+        <p>ADDITIONAL DC SITES</p>
+    </div>
     <nav class="nav justify-content-center">
         <a class="nav-link active" href="">CHARACTERS</a>
         <a class="nav-link" href="">COMICS</a>
@@ -11,5 +14,9 @@
         <a class="nav-link" href="">FANS</a>
         <a class="nav-link" href="">NEWS</a>
         <a class="nav-link" href="">SHOP</a>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
     </nav>
 </header>
