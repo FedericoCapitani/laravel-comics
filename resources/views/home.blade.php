3b4619ecc5d7b5ae52473f155dbebd1584190e4e
FedericoCapitani <federico.capitani@outlook.it>
2022-06-09 15:35:25 +0200

    add partials to layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <!-- Se necessario aggiungere un'altro segnaposto per css customizzato per pagine -->
-    @yield('custom-css')
-
-
-</head>
-
-<body>
-    <header></header>
-
-    <main>
-        @yield('content')
-    </main>
-
-    <footer></footer>
-
-    <script src="{{asset('js/app.js')}}"></script>
-    <!-- Se necessario aggiungere un'altro segnaposto per js customizzato per pagine -->
-    @yield('script-footer')
-</body>
-
-</html>
+@section('content')
+<h1>Welcome</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis illo dolorum consequuntur vitae doloremque repellendus voluptatum deleniti saepe cum nulla. </p>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+
+    <nav class="nav justify-content-center">
+        <a class="nav-link active" href="">CHARACTERS</a>
+        <a class="nav-link" href="">COMICS</a>
+        <a class="nav-link" href="">MOVIES</a>
+        <a class="nav-link" href="">TV</a>
+        <a class="nav-link" href="">GAMES</a>
+        <a class="nav-link" href="">COLLECTIBLES</a>
+        <a class="nav-link" href="">VIDEOS</a>
+        <a class="nav-link" href="">FANS</a>
+        <a class="nav-link" href="">NEWS</a>
+        <a class="nav-link" href="">SHOP</a>
+    </nav>
+</header>
