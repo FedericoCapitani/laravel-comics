918f6fa9757c6658eefe5c657ee1d624b6eb934a
FedericoCapitani <federico.capitani@outlook.it>
2022-06-10 18:14:59 +0200

    add description section to comic

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="comic_thumb">
+<div class="comic_thumb mb-4">
     <div class="container">
         <div class="thumb_container">
             <img src="{{$comic['thumb']}}" alt="">
@@ -10,8 +10,26 @@
         </div>
     </div>
 </div>
-<div class="descriptions">
-
+<div class="descriptions container mt-4">
+    <div class="row">
+        <div class="col-8 mt-4">
+            <h2 class="title text-uppercase"> {{$comic['title']}} </h2>
+            <div class="row py-4">
+                <div class="col-8 d-flex justify-content-between price_container">
+                    <div class="price white_text"> <span>U.S. Price: </span> {{$comic['price']}} </div>
+                    <div class="status">Available</div>
+                </div>
+                <div class="col-4 text-center price_container">
+                    <p class="white_text">Check Availability</p>
+                </div>
+            </div>
+            <p> {{$comic['description']}} </p>
+        </div>
+        <div class="col-4">
+            <p class="text-end">ADVERTISMENT</p>
+            <img class="pad_l_7" src="{{asset('img/adv.jpg')}}" alt="">
+        </div>
+    </div>
 </div>
 <div class="infos">
 
