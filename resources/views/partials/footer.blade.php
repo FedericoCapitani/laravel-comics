76189cd469dee40845f2192cb9d8391342521c0c
FedericoCapitani <federico.capitani@outlook.it>
2022-06-10 15:22:02 +0200

    add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,75 @@
+<footer>
+    <div class="bg_footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 pt-5 max_h_550">
+                    <div class="row">
+                        <div class="col-4">
+                            <h2>DC COMICS</h2>
+                            <a href="#" class="footer_link">Characters</a>
+                            <a href="#" class="footer_link">Comics</a>
+                            <a href="#" class="footer_link">Movies</a>
+                            <a href="#" class="footer_link">TV</a>
+                            <a href="#" class="footer_link">Games</a>
+                            <a href="#" class="footer_link">Videos</a>
+                            <a href="#" class="footer_link">News</a>
+                        </div>
+                        <div class="col-4">
+                            <h2>DC</h2>
+                            <a href="#" class="footer_link">Terms Of Use</a>
+                            <a href="#" class="footer_link">Privacy Policy</a>
+                            <a href="#" class="footer_link">Ad Choices</a>
+                            <a href="#" class="footer_link">Advertising</a>
+                            <a href="#" class="footer_link">Jobs</a>
+                            <a href="#" class="footer_link">Subscriptions</a>
+                            <a href="#" class="footer_link">Talent Workshops</a>
+                            <a href="#" class="footer_link">CPSC Certificates</a>
+                            <a href="#" class="footer_link">Ratings</a>
+                            <a href="#" class="footer_link">Shop Help</a>
+                            <a href="#" class="footer_link">Contact Us</a>
+                        </div>
+                        <div class="col-4">
+                            <h2>SITES</h2>
+                            <a href="#" class="footer_link">DC</a>
+                            <a href="#" class="footer_link">MAD Magazine</a>
+                            <a href="#" class="footer_link">DC Kids</a>
+                            <a href="#" class="footer_link">DC Universe</a>
+                            <a href="#" class="footer_link">DC Power Visa</a>
+                        </div>
+                        <div class="col-4">
+                            <h2>SHOP</h2>
+                            <a href="#" class="footer_link">Shop DC</a>
+                            <a href="#" class="footer_link">Shop DC Collectibles</a>
+                        </div>
+                        <div class="col-12 mt-4">
+                            <p class="grey_text">
+                                All Site Conente TM and 2020 DC Entertainment, unless otherwise <a href="#" class="footer_link_blue">noted here</a>. All Rights reserved.
+                                <a href="#" class="footer_link_blue">Cookie Settings</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 footer_logo max_h_550">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="footer_bottom py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <a href="#" class="sign_up_btn">SIGN-UP NOW!</a>
+                </div>
+                <div class="col-6 d-flex justify-content-end footer_socials">
+                    <a href="#" class="follow_us">FOLLOW US</a>
+                    <img src="{{asset('img/footer-facebook.png')}}" alt="">
+                    <img src="{{asset('img/footer-twitter.png')}}" alt="">
+                    <img src="{{asset('img/footer-youtube.png')}}" alt="">
+                    <img src="{{asset('img/footer-pinterest.png')}}" alt="">
+                    <img src="{{asset('img/footer-periscope.png')}}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
